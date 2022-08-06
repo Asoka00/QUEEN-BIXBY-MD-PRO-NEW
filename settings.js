19bb337e6f7ba9e5b03998978d431aac3b99d8f3
Asoka00 <110674519+Asoka00@users.noreply.github.com>
2022-08-06 09:23:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.owner = ['94760390292']
+global.premium = ['94760390292']
+global.ownernomer = '94760390292'
+global.ownername = ' pathum'
+global.botname = 'pathum-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
